dc3b58eab05d1b021145a032e80b08833065d61d
unknown <sanya@example.com>
2025-01-06 13:16:29 +0700

    add_1

---
+++ src/product.py
@@ -0,0 +1,12 @@
+class Product:
+    ''' Класс, описывающий продукты '''
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
