8899448f64882d4c2ed3438dcab5fbe022bdec59
unknown <sanya@example.com>
2025-01-07 16:03:35 +0700

    add_5

--- src/category.py
+++ src/category.py
@@ -13,3 +13,12 @@ class Category:
         self.products = products
         Category.number_categories += 1
         Category.number_products += len(products)
+
+
+# if __name__ == '__main__':
+#     xxx1 = Category("Продукты", "Мясные изделия", ["Колбаса", "Сосиски", "Сало"])
+#     print(xxx1.number_categories)
+#     print(xxx1.number_products)
+#     xxx2 = Category("Автозапчасти", "Шины", ["Летние", "Зимние", "Всесезонные"])
+#     print(xxx2.number_categories)
+#     print(xxx2.number_products)
